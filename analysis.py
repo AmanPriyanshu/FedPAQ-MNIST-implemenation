cfda64617b44a84173e6f6c02111aee9ab6dfa3b
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2020-12-31 02:36:58 +0530

    visuals

--- analysis.py
+++ analysis.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 from load_dataset import Dataset
 import os
 import time
+import matplotlib.pyplot as plt
+from PIL import Image
 
 class Test:
 	def __init__(self, train_x, train_y, test_x, test_y):
@@ -79,6 +81,37 @@ class Test:
 		all_best_performances.columns = ['local_epochs', 'r', 'precision', 'train_loss', 'train_acc', 'test_loss', 'test_acc']
 		all_best_performances.to_csv('./results/best_performances.csv', index=False)
 
+	def image_beautifier(self):
+
+		image_names = sorted(['./results/'+i for i in os.listdir('./results/') if '.png' in i])
+		for names in [image_names[i:i+4] for i in range(0, 12, 4)]:
+			images = [Image.open(x) for x in names]
+			widths, heights = zip(*(i.size for i in images))
+
+			total_width = sum(widths)
+			max_height = max(heights)
+
+			new_im = Image.new('RGB', (total_width, max_height))
+
+			x_offset = 0
+			for im in images:
+				new_im.paste(im, (x_offset,0))
+				x_offset += im.size[0]
+
+			name = names[0][len('./results/'):names[0].index('__')]
+			new_im.save(name+'_variations.png')
+
+	def image(self, performances, names, pic_name):
+		for i,name in enumerate(['train_loss', 'train_acc', 'test_loss', 'test_acc']):
+			plt.cla()
+			for j,performance in enumerate(performances):
+				plt.plot(np.arange(10), performance.T[i], label=names[j])
+			plt.legend()
+			if i%2==1:
+				plt.ylim([-0.01, 1.01])
+			plt.title(name+' - '+pic_name)
+			plt.savefig('./results/'+pic_name+'__'+name+'_analysis.png')
+
 	def image_generator(self):
 		performance = pd.read_csv('./results/best_performances.csv')
 		features = performance.columns
@@ -86,9 +119,48 @@ class Test:
 		best_index = np.argmin(performance.T[-2])
 		print("Best Performance By: ", {i:j for i,j in zip(features, performance[best_index])})
 
+		local_epochs, r, precision = performance[best_index][:3]
+		local_epochs = int(local_epochs)
+		precision = int(precision)
+
+		### Local Epochs
+		print("Analysing local_epochs with r and precision fixed to", r, precision, "respectively.")
+		performances = []
+		for local_epoch in [1, 3, 5]:
+			filename = "./results/performance_metrics/saved_models_local_epochs_"+str(local_epoch)+"_r_"+str(r).replace('.', '_')+"_precision_"+str(precision)+".csv"
+			performance = pd.read_csv(filename)
+			performance = performance.values
+			performances.append(performance)
+		performances = np.array(performances)
+		self.image(performances, names=['local_epochs='+str(i) for i in [1, 3, 5]], pic_name='local_epochs')
+
+		### r
+		print("Analysing local_epochs with local_epochs and precision fixed to", local_epochs, precision, "respectively.")
+		performances = []
+		for r_id in [0.5, 0.667, 0.833, 1.0]:
+			filename = "./results/performance_metrics/saved_models_local_epochs_"+str(local_epochs)+"_r_"+str(r_id).replace('.', '_')+"_precision_"+str(precision)+".csv"
+			performance = pd.read_csv(filename)
+			performance = performance.values
+			performances.append(performance)
+		performances = np.array(performances)
+		self.image(performances, names=['r='+str(i) for i in [0.5, 0.667, 0.833, 1.0]], pic_name='r')
+
+		### r
+		print("Analysing local_epochs with local_epochs and r fixed to", local_epochs, r, "respectively.")
+		performances = []
+		for p in [5, 6, 7]:
+			filename = "./results/performance_metrics/saved_models_local_epochs_"+str(local_epochs)+"_r_"+str(r).replace('.', '_')+"_precision_"+str(p)+".csv"
+			performance = pd.read_csv(filename)
+			performance = performance.values
+			performances.append(performance)
+		performances = np.array(performances)
+		self.image(performances, names=['precision='+str(i) for i in [5, 6, 7]], pic_name='precision')
+
+		self.image_beautifier()
+
 if __name__ == '__main__':
-	#train_x, train_y, test_x, test_y = Dataset().load_csv()
-	train_x, train_y, test_x, test_y = None, None, None, None
+	train_x, train_y, test_x, test_y = Dataset().load_csv()
+	
 	test = Test(train_x, train_y, test_x, test_y)
-	#test.analyse_all()
+	test.analyse_all()
 	test.image_generator()
