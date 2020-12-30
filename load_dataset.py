306951c26d9a4689722f62ec8432dd5b30735745
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2020-12-30 18:59:20 +0530

    training a model

--- load_dataset.py
+++ load_dataset.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
 import torch
+from tqdm import tqdm
 
 class Dataset:
 	def __init__(self):
 		self.train_x = None
 		self.train_y = None
 		self.test_x = None
+		self.batch_size = 128
 
 	def load_csv(self, path='./digits_dataset/'):
+		batch_size = self.batch_size
 		data = pd.read_csv(path + 'train.csv')
 		data = data.values
 		self.train_x = np.array([np.reshape(i, (1, int(np.sqrt(i.shape[0])), int(np.sqrt(i.shape[0])))) for i in data.T[1:].T], dtype=np.float32)
@@ -16,82 +19,15 @@ class Dataset:
 		data = pd.read_csv(path + 'test.csv')
 		data = data.values
 		self.test_x = np.array([np.reshape(i, (1, int(np.sqrt(i.shape[0])), int(np.sqrt(i.shape[0])))) for i in data], dtype=np.float32)
-		self.train_x, self.train_y, self.test_x = torch.from_numpy(self.train_x), torch.from_numpy(self.train_y), torch.from_numpy(self.test_x)
-		return self.train_x, self.train_y, self.test_x
+		data = pd.read_csv(path + 'test_results.csv')
+		data = data.values
+		self.test_y = np.array(data.T[0])
 
-class MNIST_PAQ:
-	def __init__(self):
-		self.model = None
+		self.train_x = np.array([self.train_x[n:n+batch_size] for n in range(0, len(self.train_x)-batch_size, batch_size)])/255.0
+		self.test_x = np.array([self.test_x[n:n+batch_size] for n in range(0, len(self.test_x)-batch_size, batch_size)])/255.0
+		self.train_y = np.array([self.train_y[n:n+batch_size] for n in range(0, len(self.train_y)-batch_size, batch_size)])
+		self.test_y = np.array([self.test_y[n:n+batch_size] for n in range(0, len(self.test_y)-batch_size, batch_size)])
 
-	def define_model(self):
-		self.model = torch.nn.Sequential(
-			torch.nn.Conv2d(1, 2, kernel_size=5),
-			torch.nn.ReLU(),
-			torch.nn.Conv2d(2, 4, kernel_size=7),
-			torch.nn.ReLU(),
-			torch.nn.Flatten(),
-			torch.nn.Linear(1296, 512),
-			torch.nn.ReLU(),
-			torch.nn.Linear(512, 128),
-			torch.nn.ReLU(),
-			torch.nn.Linear(128, 32),
-			torch.nn.ReLU(),
-			torch.nn.Linear(32, 10),
-			torch.nn.Sigmoid(),
-		)		
+		self.train_x, self.train_y, self.test_x, self.test_y = torch.from_numpy(self.train_x), torch.from_numpy(self.train_y), torch.from_numpy(self.test_x), torch.from_numpy(self.test_y)
 
-	def get_weights(self, dtype=np.float32, precision=7):
-		assert precision<8, "Floats generally have 6-7 significant digits. Please try a lower number than or equal to 7"
-		weights = []
-		for layer in self.model:
-			try:
-				weights.append([np.around(layer.weight.detach().numpy().astype(dtype), decimals=precision), np.around(layer.bias.detach().numpy().astype(dtype), decimals=precision)])
-			except:
-				continue
-		return np.array(weights)
-
-	def set_weights(self, weights):
-		index = 0
-		for layer_no, layer in enumerate(self.model):
-			try:
-				_ = self.model[layer_no].weight
-				self.model[layer_no].weight = torch.nn.Parameter(weights[index][0])
-				self.model[layer_no].bias = torch.nn.Parameter(weights[index][1])
-				index += 1
-			except:
-				continue
-
-	def average_weights(self, all_weights):
-		all_weights = np.array(all_weights)
-		all_weights = np.mean(all_weights, axis=0)
-		all_weights = [[torch.from_numpy(i[0].astype(np.float32)), torch.from_numpy(i[1].astype(np.float32))] for i in all_weights]
-		return all_weights
-
-	def single_client(self, dataset, weights, E):
-		self.define_model()
-		if weights is not None:
-			self.set_weights(weights)
-		for epoch in range(E):
-			for batch_x, batch_y in zip(dataset['x'], dataset['y']):
-				y_pred = self.model(batch_x)
-
-
-		pass
-
-	def single_aggregator_epoch(self, datasets, clients):
-		for dataset, client in zip(datasets, clients):
-			print(self.model(dataset['x'])[0])
-			weights_1 = self.get_weights()
-			weights_2 = self.get_weights()
-			all_weights = [weights_1, weights_2]
-			weights = self.average_weights(all_weights)
-			self.set_weights(weights)
-			print(self.model(dataset['x'])[0])
-
-		
-		
-
-train_x, train_y, test_x = Dataset().load_csv()
-
-m = MNIST_PAQ()
-m.single_client({'x':test_x, 'y':train_y}, None, 5)
+		return self.train_x, self.train_y, self.test_x, self.test_y
---
+++ train_model.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import numpy as np
+import torch
+from tqdm import tqdm
+from load_dataset import Dataset
+import os
+
+class MNIST_PAQ:
+	def __init__(self):
+		self.model = None
+		self.criterion = torch.nn.CrossEntropyLoss()
+		self.optimizer = None
+
+	def define_model(self):
+		self.model = torch.nn.Sequential(
+			torch.nn.Conv2d(1, 2, kernel_size=5),
+			torch.nn.ReLU(),
+			torch.nn.Conv2d(2, 4, kernel_size=7),
+			torch.nn.ReLU(),
+			torch.nn.Flatten(),
+			torch.nn.Linear(1296, 512),
+			torch.nn.ReLU(),
+			torch.nn.Linear(512, 128),
+			torch.nn.ReLU(),
+			torch.nn.Linear(128, 32),
+			torch.nn.ReLU(),
+			torch.nn.Linear(32, 10),
+			torch.nn.Sigmoid(),
+		)		
+
+	def get_weights(self, dtype=np.float32, precision=7):
+		assert precision<8, "Floats generally have 6-7 significant digits. Please try a lower number than or equal to 7"
+		weights = []
+		for layer in self.model:
+			try:
+				weights.append([np.around(layer.weight.detach().numpy().astype(dtype), decimals=precision), np.around(layer.bias.detach().numpy().astype(dtype), decimals=precision)])
+			except:
+				continue
+		return np.array(weights)
+
+	def set_weights(self, weights):
+		index = 0
+		for layer_no, layer in enumerate(self.model):
+			try:
+				_ = self.model[layer_no].weight
+				self.model[layer_no].weight = torch.nn.Parameter(weights[index][0])
+				self.model[layer_no].bias = torch.nn.Parameter(weights[index][1])
+				index += 1
+			except:
+				continue
+
+	def average_weights(self, all_weights):
+		all_weights = np.array(all_weights)
+		all_weights = np.mean(all_weights, axis=0)
+		all_weights = [[torch.from_numpy(i[0].astype(np.float32)), torch.from_numpy(i[1].astype(np.float32))] for i in all_weights]
+		return all_weights
+
+	def client_generator(self, train_x, train_y, number_of_clients=100):
+		size = train_y.shape[0]//number_of_clients
+		train_x, train_y = train_x.numpy(), train_y.numpy()
+		train_x = np.array([train_x[i:i+size] for i in range(0, len(train_x)-len(train_x)%size, size)])
+		train_y = np.array([train_y[i:i+size] for i in range(0, len(train_y)-len(train_y)%size, size)])
+		train_x = torch.from_numpy(train_x)
+		train_y = torch.from_numpy(train_y)
+		return train_x, train_y
+
+	def single_client(self, dataset, weights, E):
+		self.define_model()
+		if weights is not None:
+			self.set_weights(weights)
+		self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.01, momentum=0.9)
+		for epoch in range(E):
+			running_loss = 0
+			for batch_x, target in zip(dataset['x'], dataset['y']):
+				output = self.model(batch_x)
+				loss = self.criterion(output, target)
+				self.optimizer.zero_grad()
+				loss.backward()
+				self.optimizer.step()
+				running_loss += loss.item()
+			running_loss /= len(dataset['y'])
+		weights = self.get_weights()
+		return weights, running_loss
+
+	def test_aggregated_model(self, test_x, test_y, epoch):
+		os.system('mkdir saved_models')
+		acc = 0
+		with torch.no_grad():
+			for batch_x, batch_y in zip(test_x, test_y):
+				y_pred = self.model(batch_x)
+				y_pred = torch.argmax(y_pred, dim=1)
+				acc += torch.sum(y_pred == batch_y)/y_pred.shape[0]
+		torch.save(self.model, "./saved_models/model_epoch_"+str(epoch)+".pt")
+		return (acc/test_x.shape[0])
+			
+
+	def train_aggregator(self, datasets, datasets_test, aggregate_epochs=10, local_epochs=5):
+		E = local_epochs
+		aggregate_weights = None
+		for epoch in range(aggregate_epochs):
+			all_weights = []
+			client = 0
+			running_loss = 0
+			clients = tqdm(zip(datasets['x'], datasets['y']), total=datasets['x'].shape[0])
+			for dataset_x, dataset_y in clients:
+				dataset = {'x':dataset_x, 'y':dataset_y}
+				weights, loss = self.single_client(dataset, aggregate_weights, E)
+				running_loss += loss
+				all_weights.append(weights)
+				client += 1
+				clients.set_description(str({"Epoch":epoch,"Loss": round(running_loss/client, 5)}))
+				clients.refresh()
+			aggregate_weights = self.average_weights(all_weights)
+			self.set_weights(aggregate_weights)
+			test_acc = self.test_aggregated_model(datasets_test['x'], datasets_test['y'], epoch)
+			print("Test Accuracy:", round(test_acc.item(), 5))
+			clients.close()
+
+if __name__ == '__main__':
+	number_of_clients = 150
+	aggregate_epochs = 100
+	local_epochs = 3
+
+	train_x, train_y, test_x, test_y = Dataset().load_csv()
+
+	m = MNIST_PAQ()
+	train_x, train_y = m.client_generator(train_x, train_y, number_of_clients=number_of_clients)
+	m.train_aggregator({'x':train_x, 'y':train_y}, {'x':test_x, 'y':test_y}, aggregate_epochs=aggregate_epochs, local_epochs=local_epochs)
